24a14a4d2f1577b1e55c9e0ab9bc60c0a10f864b
Ilie Gabuja <iliegabujabike@gmail.com>
2025-05-25 12:39:44 +0000

    Update reuse.c

--- reuse.c
+++ reuse.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /*using multiple functions, say hi , askname and welcome functions*/
 void sayHi() {
@@ -11,14 +10,21 @@ void askName() {
 }
 
 void welcome(char name[]) { /*here the parameter takes a string value which you input when the main() is run*/
-    printf("Welcome %s!", name);
+    printf("Welcome %s!\n", name);
+}
+
+void age(int age) { /*Now same for int*/
+    printf("What age are you?\n");
 }
 int main() {
     char name[20];
+    int agee;
     sayHi();
     askName();
     scanf("%s", &name);
     welcome(name); /*the function takes the value of the inputted thing*/
+    age(agee);
+    scanf("%d", agee);
     return 0;
 }
 
