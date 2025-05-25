8427a22f52a77afc7ea03779d7ad9309491d40ff
Ilie Gabuja <iliegabujabike@gmail.com>
2025-05-25 10:20:08 +0000

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,13 +2,15 @@
 
 int main() {
     int nr;
+    const int nr2 = 3;
+    const int nr3 = 4;
     printf("Please enter a number!\n");
     scanf("%d", &nr);
-    if (nr % 3 == 0 & nr % 4 == 0) {
+    if (nr % nr2 == 0 & nr % nr3 == 0) {
         printf("FizzBuzz!");
-    }   else if (nr % 3 == 0) {
+    }   else if (nr % nr2 == 0) {
         printf("Fizz!");
-    }   else if (nr % 4 == 0) {
+    }   else if (nr % nr3 == 0) {
         printf("Buzz!");
     }   else {
         printf("Not a Fizz or Buzz or Fizzbuzz number!");
